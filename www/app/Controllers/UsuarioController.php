79635067d31d102e54ff4cae51de7ac41cf034c0
Nachoztv <nachocampino@gmail.com>
2024-11-05 13:02:44 +0100

    Martes 5 noviembre clase

--- www/app/Controllers/UsuarioController.php
+++ www/app/Controllers/UsuarioController.php
@@ -3,6 +3,7 @@ declare (strict_types=1);
 namespace Com\Daw2\Controllers;
 
 use Com\Daw2\Core\BaseController;
+use Decimal\Decimal;
 
 class UsuarioController extends BaseController
 {
@@ -16,8 +17,18 @@ class UsuarioController extends BaseController
         $data = array('titulo' => 'Usuarios',
             'breadcumb' => array('Inicio' => array('url' => '#', 'active' => false)),
         );
-        $data['usuarios'] = $model->getUsers();
+        $usuarios = $model->getUsers();
+        $data['usuarios'] = $this->calcularNeto($usuarios);
         $this->view->showViews(array('templates/header.view.php', 'UsuarioView.view.php', 'templates/footer.view.php'), $data);
+    }
+    private function calcularNeto(array $usuarios): array{
+        foreach ($usuarios as &$usuario){
+            $salarioBruto = new Decimal($usuario['salarioBruto']);
+            $retencionIRPF = new Decimal($usuario['retencionIRPF'],2);
+            $neto = $salarioBruto - ($salarioBruto * $retencionIRPF/new Decimal('100',2));
+            $usuario['salarioNeto'] = $neto->toFixed(2,true,Decimal::ROUND_HALF_UP);
+        }
+        return $usuarios;
     }
     public function showUsersBruto(): void
     {
--- www/app/Views/UsuarioView.view.php
+++ www/app/Views/UsuarioView.view.php
@@ -1,3 +1,27 @@
+<div class="col-12">
+    <div class="card shadow mb-4">
+        <form method="post">
+            <input type="hidden" name="order" value="1"/>
+            <div
+                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                <h6 class="m-0 font-weight-bold text-primary">Filtro</h6>
+            </div>
+            <!-- Card Body -->
+            <div class="card-body">
+                <!--<form action="./?sec=formulario" method="post">                   -->
+                <div class="row">
+                    <div class="col-12 col-lg-4">
+                        <div class="mb-3">
+                            <label for="user">Username:</label>
+                            <input type="text" class="form-control" name="user" id="user" value="" />
+                            <p class = "text-danger small" ><?php echo $errors['username'] ?? '' ;?></p>
+                        </div>
+                    </div>
+                    <input type="submit" name="submit" class="btn btn-primary " value="Filtrar"/>
+        </form>
+    </div>
+</div>
+</div>
 <table id="csvTable" class="table table-striped">
     <thead>
     <tr>
@@ -19,7 +43,7 @@
                     <td><?php echo number_format($user['retencionIRPF'],0); ?>%</td>
                     <td><?php echo $user['id_rol']; ?></td>
                     <td><?php echo $user['id_country']; ?></td>
-                    <td><?php echo number_format(($user['salarioBruto'])-($user['salarioBruto']*$user['retencionIRPF']/100),2,',','.'); ?></td>
+                    <td><?php echo str_replace([',','.','_'],['_',',','.'],$user['salarioNeto']);?></td>
             </tr>
             <?php
         }
