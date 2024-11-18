191fbbe757120370c8d1e3a530040aca8e6a9250
Nachoztv <nachocampino@gmail.com>
2024-11-18 18:28:57 +0100

    Lunes 18 noviembre clase 2

--- www/app/Views/formUsuarioView.view.php
+++ www/app/Views/formUsuarioView.view.php
@@ -1,11 +1,10 @@
 <div class="row">
     <div class="col-12">
         <div class="card shadow mb-4">
-            <form method="post">
-                <input type="hidden" name="order" value="1"/>
+            <form method="get" action="">
                 <div
                         class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Insercion</h6>
+                    <h6 class="m-0 font-weight-bold text-primary">Filtros</h6>
                 </div>
                 <!-- Card Body -->
                 <div class="card-body">
@@ -13,82 +12,81 @@
                     <div class="row">
                         <div class="col-12 col-lg-4">
                             <div class="form-group">
-                                <label for="user">Username:</label>
-                                <input type="text" class="form-control" name="user" id="user"
-                                       value="<?php echo $_GET['user'] ?? ''; ?>"/>
-                                <p class="text-danger small"><?php echo $errors['user'] ?? ''; ?></p>
+                                <label for="username">Username:</label>
+                                <input
+                                        type="text"
+                                        class="form-control"
+                                        name="username"
+                                        id="username"
+                                        value="<?php echo $input['username'] ?? ''; ?>"
+                                />
                             </div>
                         </div>
                         <div class="col-12 col-lg-3">
                             <div class="form-group">
-                                <label for="retencionIRPF">Tipo De IRPF:</label>
-                                <br>
-                                <select name="retencionIRPF" id="retencionIRPF">
+                                <label for="id_rol">Tipo:</label>
+                                <select name="id_rol" id="id_rol" class="form-control">
                                     <option value="">-</option>
-                                    <?php
-                                    foreach ($tiposIrpf as $irpf) {
+                                    <?php foreach ($roles as $role) {
                                         ?>
-                                        <option value="<?php echo $irpf['retencionIRPF']; ?>" <?php echo (isset($input['retencionIRPF']) && $irpf['retencionIRPF'] == $input['retencionIRPF']) ? 'selected' : ''; ?>> <?php echo ucfirst($irpf['retencionIRPF']) ?></option>
+                                        <option value="<?php echo $role['id_rol'] ?>" <?php echo (isset($input['id_rol']) && $role['id_rol'] == $input['id_rol']) ? 'selected' : ''; ?>><?php echo ucfirst($role['nombre_rol']); ?></option>
                                         <?php
                                     }
                                     ?>
                                 </select>
-                                <p class="text-danger small"><?php echo $errors['retencionIRPF'] ?? ''; ?></p>
                             </div>
                         </div>
                         <div class="col-12 col-lg-4">
                             <div class="form-group">
-                                <label for="id_rol">Tipo De Rol:</label>
-                                <br>
-                                <select name="id_rol" id="id_rol">
-                                    <option value="">-</option>
-                                    <?php
-                                    foreach ($tiposRol as $rol) {
-                                        ?>
-                                        <option value="<?php echo $rol['id_rol'] ?>" <?php echo (isset($input['id_rol']) && $rol['id_rol'] == $input['id_rol']) ? 'selected' : ''; ?>> <?php echo ucfirst($rol['nombre_rol']) ?></option>
-                                        <?php
-                                    }
-                                    ?>
-                                </select>
-                                <p class="text-danger small"><?php echo $errors['id_rol'] ?? ''; ?></p>
+                                <label for="salario_min">Salario:</label>
+                                <div class="row">
+                                    <div class="col-6">
+                                        <input type="text" class="form-control" name="min_salar" id="min_salar" value="<?php echo $input['min_salar'] ?? ''; ?>" placeholder="Mínimo" />
+                                    </div>
+                                    <div class="col-6">
+                                        <input type="text" class="form-control" name="max_salar" id="max_salar" value="<?php echo $input['max_salar'] ?? ''; ?>" placeholder="Máximo" />
+                                    </div>
+                                </div>
                             </div>
                         </div>
+
                         <div class="col-12 col-lg-4">
                             <div class="form-group">
-                                <label for="salMin">Salario Minimo:</label>
-                                <input type="number" class="form-control" name="salMin" id="salMin"
-                                       value="<?php echo $_GET['salMin'] ?? ''; ?>"/>
-                                <label for="salMax">Salario Maximo:</label>
-                                <input type="number" class="form-control" name="salMax" id="salMax"
-                                       value="<?php echo $_GET['salMax'] ?? ''; ?>"/>
+                                <label for="retencion_min">Retención:</label>
+                                <div class="row">
+                                    <div class="col-6">
+                                        <input type="text" class="form-control" name="min_retencion" id="min_retencion" value="<?php echo $input['min_retencion'] ?? ''; ?>" placeholder="Mínimo" />
+                                    </div>
+                                    <div class="col-6">
+                                        <input type="text" class="form-control" name="max_retencion" id="max_retencion" value="<?php echo $input['max_retencion'] ?? ''; ?>" placeholder="Máximo" />
+                                    </div>
+                                </div>
                             </div>
-                            <p class="text-danger small"><?php echo $errors['salary'] ?? ''; ?></p>
                         </div>
-                        <div class="col-12 col-lg-4">
-                            <div class="mb-3">
-                                <label for="id_country">Nacionalidad:</label>
-                                <br>
-                                <select name="id_country" id="id_country">
-                                    <option value="">-</option>
+                        <div class="col-12 col-lg-3">
+                            <div class="form-group">
+                                <label for="id_country">Tipo:</label>
+                                <select name="id_country[]" id="id_country" class="form-control select2" data-placeholder="Países" multiple>
                                     <?php
                                     foreach ($countries as $country) {
                                         ?>
-                                        <option value="<?php echo $country['country_name'] ?>" <?php echo (isset($input['id_country']) && $country['id_country'] == $input['id_country']) ? 'selected' : ''; ?>> <?php echo ucfirst($country['country_name']) ?></option>
+                                        <option value="<?php echo $country['id']; ?>" <?php echo (isset($input['id_country']) && in_array($country['id'], $input['id_country'])) ? 'selected' : ''; ?>>
+                                            <?php echo $country['country_name']; ?>
+                                        </option>
                                         <?php
                                     }
                                     ?>
                                 </select>
-                                <p class="text-danger small"><?php echo $errors['id_country'] ?? ''; ?></p>
                             </div>
                         </div>
-                        <div class="card-footer">
-                            <div class="col-12 text-right">
-                            <input type="submit" name="submit" class="btn btn-primary " value="Insertar"/>
-                            </div>
-                        </div>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <div class="col-12 text-right">
+                        <a href="<?php echo $_ENV['host.folder']; ?>usuarios-filtro" value="" name="reiniciar" class="btn btn-danger">Reiniciar filtros</a>
+                        <input type="submit" value="Aplicar filtros" class="btn btn-primary ml-2"/>
+                    </div>
+                </div>
             </form>
         </div>
     </div>
-</div>
-</div>
-</div>
